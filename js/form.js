723c13dc068e789e6e892cf298bb904df6efbb95
Tatsiana Yasiuchenia <tsbarankina@gmail.com>
2021-06-19 14:31:53 +0300

    feat: added a function to activate / deactivate the form

--- js/form.js
+++ js/form.js
@@ -1,1 +1,33 @@
 // Реализация формы: заполнение, условия заполнения
+
+const activation = (elemClass, isActive) => {
+  const element = document.querySelector(elemClass);
+
+  if (isActive) {
+    if (element.classList.contains(`${elemClass}--disabled`)) {
+      element.classList.remove(`${elemClass}--disabled`);
+    }
+
+    const elems = element.querySelectorAll('select, input, textarea, button');
+    for (let i = 0; i < elems.length-1; i++) {
+      if (elems[i].hasAttribute('disabled')) {
+        elems[i].removeAttribute('disabled', 'disabled');
+      }
+    }
+  } else {
+    if (!element.classList.contains(`${elemClass}--disabled`)) {
+      element.classList.add(`${elemClass}--disabled`);
+    }
+    const elems = element.querySelectorAll('select, input, button');
+    for (let i = 0; i < elems.length-1; i++) {
+      if (!elems[i].hasAttribute('disabled')) {
+        elems[i].setAttribute('disabled', 'disabled');
+      }
+    }
+  }
+};
+
+activation('.ad-form', false);
+activation('.map__filters', false);
+
+export{activation};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Точка входа
 import {generateAdMarkup} from './card.js';
 import {similarAds} from './ads.js';
+import{activation} from './form.js';
 
 generateAdMarkup(similarAds);
-
+activation('.ad-form', false);
+activation('.map__filters', false);
