63b35afed0e09ce8cfd0e535f21090195104357f
Tatsiana Yasiuchenia <tsbarankina@gmail.com>
2021-06-27 15:39:58 +0300

    fix: added functions to imports

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 // Точка входа
-import {activation, deactivation, FORM_CLASS, MAP_FILTERS_CLASS, formValidity} from './form.js';
+import {deactivation, FORM_CLASS, MAP_FILTERS_CLASS, formValidity} from './form.js';
+import {setAddress, addInteractiveMap, TOKIO_CENTER, addressInput, mapClass, setMainPinOnMap, resetButton, setSimplePinsOnMap, similarAds} from './map.js';
 import './map.js';
 
 deactivation(FORM_CLASS);
 deactivation(MAP_FILTERS_CLASS);
-activation(FORM_CLASS);
-activation(MAP_FILTERS_CLASS);
+setAddress(addressInput, TOKIO_CENTER);
+const interactiveMap = addInteractiveMap(mapClass, TOKIO_CENTER);
+setMainPinOnMap(interactiveMap, TOKIO_CENTER, addressInput, resetButton);
+setSimplePinsOnMap(similarAds, interactiveMap);
 formValidity();
--- js/map.js
+++ js/map.js
@@ -4,97 +4,112 @@ import {createAds} from './ads.js';
 import {generateAdMarkup} from './card.js';
 
 const TOKIO_CENTER = {
-  LAT: 35.6894,
-  LNG: 139.692,
+  LAT: 35.41,
+  LNG: 139.41,
 };
 
 const TOTAL_POINTS = 10;
+
+const mapClass = 'map-canvas';
 
 const resetButton = document.querySelector('.ad-form__reset');
 
 const addressInput = document.querySelector('#address');
 
-addressInput.value = `${TOKIO_CENTER.LAT}, ${TOKIO_CENTER.LNG}`;
-addressInput.setAttribute('disabled', 'disabled');
+const setAddress = (changingInput, centerCoords) => {
+  changingInput.value = `${centerCoords.LAT}, ${centerCoords.LNG}`;
+  changingInput.setAttribute('disabled', 'disabled');
+};
 
-const map = L.map('map-canvas')
-  .on('load', () => {
-    activation(FORM_CLASS);
-    activation(MAP_FILTERS_CLASS);
-  })
-  .setView({
-    lat: TOKIO_CENTER.LAT,
-    lng: TOKIO_CENTER.LNG,
-  }, 16);
+const addInteractiveMap = (classForMap, centerCoords) => {
+  const map = L.map(classForMap)
+    .on('load', () => {
+      activation(FORM_CLASS);
+      activation(MAP_FILTERS_CLASS);
+    })
+    .setView({
+      lat: centerCoords.LAT,
+      lng: centerCoords.LNG,
+    }, 16);
 
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
 
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(map);
+  return map;
+};
 
-const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
+const setMainPinOnMap = (intMap, centerCoords, inputWithAddress, buttonThatResets) => {
 
-const mainPinMarker = L.marker(
-  {
-    lat: TOKIO_CENTER.LAT,
-    lng: TOKIO_CENTER.LNG,
-  },
-  {
-    draggable: true,
-    icon: mainPinIcon,
-  },
-).on('move', (evt) => {
-  addressInput.value = `${evt.latlng.lat.toFixed(5)}, ${evt.latlng.lng.toFixed(5)}`;
-});
-
-mainPinMarker.addTo(map);
-
-mainPinMarker.on('moveend', (evt) => {
-  evt.target.getLatLng();
-});
-
-resetButton.addEventListener('click', () => {
-  mainPinMarker.setLatLng({
-    lat: TOKIO_CENTER.LAT,
-    lng: TOKIO_CENTER.LNG,
+  const mainPinIcon = L.icon({
+    iconUrl: '../img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
   });
-  map.setView({
-    lat: TOKIO_CENTER.LAT,
-    lng: TOKIO_CENTER.LNG,
-  }, 16);
-});
 
-const simplePinIcon = L.icon({
-  iconUrl: '../img/pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-});
+  const mainPinMarker = L.marker(
+    {
+      lat: centerCoords.LAT,
+      lng: centerCoords.LNG,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  ).on('move', (evt) => {
+    inputWithAddress.value = `${evt.latlng.lat.toFixed(5)}, ${evt.latlng.lng.toFixed(5)}`;
+  });
+
+  mainPinMarker.addTo(intMap);
+
+  mainPinMarker.on('moveend', (evt) => {
+    evt.target.getLatLng();
+  });
+
+  buttonThatResets.addEventListener('click', () => {
+    mainPinMarker.setLatLng({
+      lat: centerCoords.LAT,
+      lng: centerCoords.LNG,
+    });
+    intMap.setView({
+      lat: centerCoords.LAT,
+      lng: centerCoords.LNG,
+    }, 16);
+  });
+};
 
 const similarAds = new Array(TOTAL_POINTS).fill(null).map(createAds);
 
-similarAds.forEach(({location: { lat, lng }}) => {
-  const marker = L.marker({
-    lat,
-    lng,
-  },
-  {
-    draggable: true,
-    icon: simplePinIcon,
-  },
-  );
+const setSimplePinsOnMap = (adsArray, intMap) => {
 
-  marker
-    .addTo(map)
-    .bindPopup(generateAdMarkup(),
-      {
-        keepInView: true,
-      },
+  const simplePinIcon = L.icon({
+    iconUrl: '../img/pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  adsArray.forEach(({location: { lat, lng }}) => {
+    const marker = L.marker({
+      lat,
+      lng,
+    },
+    {
+      draggable: true,
+      icon: simplePinIcon,
+    },
     );
-});
+
+    marker
+      .addTo(intMap)
+      .bindPopup(generateAdMarkup(),
+        {
+          keepInView: true,
+        },
+      );
+  });
+};
+
+export {setAddress, addInteractiveMap, setMainPinOnMap, setSimplePinsOnMap, TOKIO_CENTER, addressInput, mapClass, resetButton, similarAds};
